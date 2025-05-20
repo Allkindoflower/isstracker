ee9fe2cd42c71f3859c472c7fd8e53914bf4c7be
Uğur Baştuğ <bastugugur85@gmail.com>
2025-05-20 21:04:16 +0300

    Failed to commit the updated code, second attempt

--- isslocation.py
+++ isslocation.py
@@ -1,13 +1,16 @@
 import requests
 import time
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def getIssLocation():   
-    url_iss = 'http://api.open-notify.org/iss-now.json'
+def getIssLocation():  
+    url_iss = os.getenv('url_iss')
     response_iss = requests.get(url_iss)
     data_iss = response_iss.json()
     latitude = data_iss['iss_position']['latitude']
@@ -16,13 +19,13 @@ def getIssLocation():
          
 
 def findCountry(latitude, longitude):
-    url_reversegeo = f'https://us1.locationiq.com/v1/reverse?key=pk.4455fb17e19dec3d3a5bd7e45ef70c58&lat={latitude}&lon={longitude}&format=json&'
+    key_reversegeo = os.getenv('key_reversegeo')
+    url_reversegeo = f'https://us1.locationiq.com/v1/reverse?key={key_reversegeo}&lat={latitude}&lon={longitude}&format=json&'
     response_geo = requests.get(url_reversegeo)
     data_geo = response_geo.json()
     return data_geo
 
-while True:
-    
+while True: 
     try:
         latitude, longitude = getIssLocation()
         data_geo = findCountry(latitude, longitude)   
@@ -33,7 +36,7 @@ while True:
             print('Ocean/Sea')
     except (KeyError, ValueError, TypeError, requests.exceptions.RequestException):
         print('Cannot reach the location information.')
-    time.sleep(20)
+    time.sleep(2)
     clear_terminal()
     
     
